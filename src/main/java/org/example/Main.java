606c8e2b5774fc2975048f7687bf1e21b5d6e8a9
Theara Seng <me@thearaseng.com>
2024-04-05 14:45:44 -0500

    sort contact list by last name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ import org.example.model.Contact;
 import org.example.repository.ContactRepository;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -23,6 +25,11 @@ public class Main {
                 .writer().withDefaultPrettyPrinter();
 
         Contact[] contacts = ContactRepository.getAllContacts();
+
+        Arrays.sort(contacts,
+                Comparator.comparing(Contact::getLastName)
+        );
+
         String json = ow.writeValueAsString(contacts);
         System.out.println(json);
     }
